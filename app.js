6dc0ff647d4595cf22cadaa2a5631b60585bdf02
Dave Horton <daveh@beachdognet.com>
2017-05-30 20:43:58 -0400

    fix uri parsing bugs, pass on add'l custom headers

--- app.js
+++ app.js
@@ -6,13 +6,26 @@ var Srf = require('drachtio-srf') ;
 var srf = new Srf(app) ;
 var blacklist = require('./lib/blacklist');
 var config = require('./lib/config') ;
+var _ = require('lodash');
 
 
 exports = module.exports = app ;
 
 srf.connect(config.drachtio) 
 .on('connect', function(err, hostport) {
+  app.hostport = hostport ;
+  app.transports = _.map( hostport.split(','), function(s) {
+    var arr = /(.*)\/(.*)/.exec( s ) ;
+    if( arr ) {
+      return {
+        protocol: arr[1],
+        address: arr[2]
+      } ;
+    }
+    return {} ;
+  });
   console.log('connected to drachtio listening for SIP on %s', hostport) ;
+  console.log('transports %s', JSON.stringify(app.transports)) ;
 })
 .on('error', function(err){
   console.error('Error connecting to drachtio server: ', err.message ) ;
